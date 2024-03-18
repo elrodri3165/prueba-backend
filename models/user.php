f98f998bfe44a1eb53b4ea535b042d3685f88306
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2024-03-18 17:17:36 -0300

    create models

---
+++ models/user.php
@@ -0,0 +1,153 @@
+<?php
+use core\clases\Model;
+
+class user extends Model{
+
+    protected int $id;
+    protected string $fullname;
+    protected string $email;
+    protected string $pass;
+    protected string $openid;
+    protected DateTime $creation_date;
+    protected DateTime $update_date_datetime;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of fullname
+     */ 
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    /**
+     * Set the value of fullname
+     *
+     * @return  self
+     */ 
+    public function setFullname($fullname)
+    {
+        $this->fullname = $fullname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of pass
+     */ 
+    public function getPass()
+    {
+        return $this->pass;
+    }
+
+    /**
+     * Set the value of pass
+     *
+     * @return  self
+     */ 
+    public function setPass($pass)
+    {
+        $this->pass = $pass;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of openid
+     */ 
+    public function getOpenid()
+    {
+        return $this->openid;
+    }
+
+    /**
+     * Set the value of openid
+     *
+     * @return  self
+     */ 
+    public function setOpenid($openid)
+    {
+        $this->openid = $openid;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of creation_date
+     */ 
+    public function getCreation_date()
+    {
+        return $this->creation_date;
+    }
+
+    /**
+     * Set the value of creation_date
+     *
+     * @return  self
+     */ 
+    public function setCreation_date($creation_date)
+    {
+        $this->creation_date = $creation_date;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of update_date_datetime
+     */ 
+    public function getUpdate_date_datetime()
+    {
+        return $this->update_date_datetime;
+    }
+
+    /**
+     * Set the value of update_date_datetime
+     *
+     * @return  self
+     */ 
+    public function setUpdate_date_datetime($update_date_datetime)
+    {
+        $this->update_date_datetime = $update_date_datetime;
+
+        return $this;
+    }
+}
---
+++ models/user_comment.php
@@ -0,0 +1,132 @@
+<?php
+use core\clases\Model;
+
+class user_comment extends Model{
+
+    protected int $id;
+    protected int $user;
+    protected string $coment_text;
+    protected int $likes;
+    protected DateTime $creation_date;
+    protected DateTime $update_date;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user
+     */ 
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set the value of user
+     *
+     * @return  self
+     */ 
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of coment_text
+     */ 
+    public function getComent_text()
+    {
+        return $this->coment_text;
+    }
+
+    /**
+     * Set the value of coment_text
+     *
+     * @return  self
+     */ 
+    public function setComent_text($coment_text)
+    {
+        $this->coment_text = $coment_text;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of likes
+     */ 
+    public function getLikes()
+    {
+        return $this->likes;
+    }
+
+    /**
+     * Set the value of likes
+     *
+     * @return  self
+     */ 
+    public function setLikes($likes)
+    {
+        $this->likes = $likes;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of creation_date
+     */ 
+    public function getCreation_date()
+    {
+        return $this->creation_date;
+    }
+
+    /**
+     * Set the value of creation_date
+     *
+     * @return  self
+     */ 
+    public function setCreation_date($creation_date)
+    {
+        $this->creation_date = $creation_date;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of update_date
+     */ 
+    public function getUpdate_date()
+    {
+        return $this->update_date;
+    }
+
+    /**
+     * Set the value of update_date
+     *
+     * @return  self
+     */ 
+    public function setUpdate_date($update_date)
+    {
+        $this->update_date = $update_date;
+
+        return $this;
+    }
+}
