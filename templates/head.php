2d27af1833f4fec784bebb287500f114b6595c3c
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2024-03-18 16:56:12 -0300

    ajustes finales

--- templates/head.php
+++ templates/head.php
@@ -13,28 +13,20 @@
     <link href="{ruta}static/img/favicon.ico?{version}" type="image/x-icon" rel="icon" />
     
     <!-- styles RG-->
-    <link rel="stylesheet" href="{ruta}static/css/stylesWM.css?{version}">
+    <link rel="stylesheet" href="{ruta}static/css/styles.css?{version}">
     <!-- end styles RG-->
-
-    <!-- Jquery-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 
     <!--css boostrap icons-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
     <!--fin de css boostrap icons-->
 
-    <!-- CSS de boostrap V:5.1.3-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
-    <!-- JavaScript Bundle with Popper V:5.1.3 -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-    <!--css boostrap icons V:1.5.0-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <!-- CSS de boostrap V:5.3.3-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper V:5.3.3 -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!--css boostrap icons V:1.11.3-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
    
-    <!--CSS LIGHTBOX GALLERY-->
-    <link href="{ruta}static/css/lightbox.css?{version}" rel="stylesheet" />
-    
     <!-- SWEET ALERT-->
     <script src="{ruta}static/js/sweetalert/sweetalert.js?"></script>
 
