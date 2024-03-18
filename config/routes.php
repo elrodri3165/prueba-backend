45b5aa877d4ad90aa98ed665892fd0ba391c42a0
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2024-03-18 15:53:07 -0300

    updates

--- config/routes.php
+++ config/routes.php
@@ -1,31 +1,4 @@
 <?php
-/*
-VERSION 4.0 MEJORA DE LAS RUTAS. 
-
-DECLARAR LAS RUTAS ACA
-NO MODIFICAR EL NOMBRE DE LA VARIABLE
-
-DECLRAR LA RUTA Y EL NOMBRE DE DEL CONTROLADOR
-QUE DEBE LLAMAR Y SU MÉTODO. 
-
-SE PUDEN UTILIZAR CONTROLADORES NUEVOS, SOLAMENTE 
-SE DEBEN CREAR DENTRO DE LA CARPETA functions/clases
-Y SU NOMBRE DEBE SER COMO UNA CLASE. 
-EJ. 
-
-MiClase.php --> class MiClase{
-
-}
-
-DE ESTA MANERA SE HACE EL REQUIRE AUTOMÁTICAMENTE.
-
-***UTILIZAR DE ESTA MANENRA ***
-"AQUI RUTA" => [
-        "CONTROLADOR" => "MÉTODO"
-    ],
-
-*/
-
 
 $routes_controlador = [
     
@@ -38,26 +11,6 @@ $routes_controlador = [
     "index" => [
         "ControladorVista" => "Index"
     ],
-    "admin" => [
-        "ControladorVista" => "Admin"
-    ],
-    "registrar-usuario" => [
-        "ControladorVista" => "RegistrarUsuario"
-    ],
-    
 
-    
-    "crear-usuario" => [
-        "ControladorLogica" => "CrearUsuario"
-    ],
-    "cerrar-sesion" => [
-        "ControladorLogica" => "CerrarSesion"
-    ],
-    "validarlogin" => [
-        "ControladorLogica" => "ValidarLogin"
-    ],
-    "subir-archivo" => [
-        "ControladorLogica" => "SubirArchivos"
-    ],
     
 ];
--- controladores/ControladorVista.php
+++ controladores/ControladorVista.php
@@ -29,7 +29,7 @@ class ControladorVista{
     
     
     public function Index(){
-    
+        echo 'estoy en index';
     }
     
     
