d5021188149e889109d3c6eece3b634f29539506
elrodri3165 <74930104+elrodri3165@users.noreply.github.com>
2024-03-19 07:48:40 -0300

    datos innecesarios

--- config/config_site.php
+++ config/config_site.php
@@ -1,5 +1,4 @@
 <?php
-
 /*DATOS DE CONFIGURACION DEL PROYECTO*/
 date_default_timezone_set           ('America/Argentina/Buenos_Aires');
 setlocale(LC_TIME                   ,'es_ES.UTF8');
@@ -9,18 +8,9 @@ ini_set('display_startup'           ,1);
 error_reporting                     (E_ALL);
 /*FIN DATOS DE CONFIGURACION DEL PROYECTO*/
 
-/*CONSTANTES*/
-define ('MANTENIMIENTO'             , false);
-define ('PRODUCCION'                , false);
-define ('VERSION'                   , '1.0');
-
-/*VERSION DEL PROYECTO*/
-
-define ('SERVER'                    ,$_SERVER['SERVER_NAME']);
-
-define('VERSION_VM'                 ,'7.0'); /*ACTUALIZADA el 29/12/2023**/
-define('APP_NOMBRE'                 ,'WebMaker');
-
+define('VERSION'                    , '1.0');
+define('SERVER'                     ,$_SERVER['SERVER_NAME']);
+define('APP_NOMBRE'                 ,'PruebaBackEnd');
 define('DS'                         , DIRECTORY_SEPARATOR);
 define('ROOT'                       , getcwd().DS);
 define('APP'                        , ROOT.'core'.DS);
@@ -35,29 +25,13 @@ define ("ERRORES_POR_CONSOLAJS"     ,true);
 define ("ERRORES_PERSONALIZADOS"    ,true);
 /******CONFIGURACION DE GESTION DE ERRORES*********/
 
-/*FIN CONSTANTES*/
+define ('URL'                       ,'http://localhost:8080/');
 
-/************************************AMBIENTE DE PRODUCCION***********************************************/
-if (PRODUCCION){
-     /*URL PARA RUTAS*/
-     define ('URL'                  ,'http://localhost:8080/'); //DEFINIR!
-    
-     /*CONTSTANES DE CONEXION DB PRUDUCCION*/
-    define('SERVIDOR'              ,'localhost');
-    define('USUARIO'               ,'');
-    define('CLAVE'                 , '');
-    define('BASE'                  ,'prueba');
-}
-/************************************AMBIENTE DE PRODUCCION***********************************************/
 
-/*************************************AMBIENTE DE TESTING*************************************************/
-else{
-    define ('URL'                   ,'http://localhost:8080/'); //DEFINIR!
-    
+/*************************************DATABASE*************************************************/ 
     /*CONTSTANES DE CONEXION DB*/
     define('SERVIDOR'               ,'mariadb');
     define('USUARIO'                ,'prueba_web');
     define('CLAVE'                  ,'123456');
     define('BASE'                   ,'prueba');
-}
-/*************************************AMBIENTE DE TESTING*************************************************/
+/*************************************DATABASE*************************************************/
